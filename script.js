3c94ab47b9adc8a4d6366bf1f8725e524333b31d
bigneuzinn <160941317+bigneuzinn@users.noreply.github.com>
2024-07-25 16:47:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var nome = "Vinicius"
+var sobrenome = "Lucas"
+
+var numero1 = 10 
+var numero2 = 10
+
+var num1 = parseInt(prompt("Digite um numero"))
+var num2 = parseInt(prompt("Digite outro numero"))
+var soma = num1 + num2
+var subtracao = num1 - num2
+var multiplicacao = num1 * num2
+var divisao = num1 / num2
+console.log("soma é: " + soma)
+
+//Imprimir 
+document.write(nome)
+
+console.log("Numero 1: " + num1)
+console.log("Numero 2: " + num2)
+console.log("Soma é " + soma)
+
+
+console.log("nome:" + nome ) 
+console.log("sobrenome: " + sobrenome)
+console.log(numero1 + numero1)
